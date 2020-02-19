308011b3f1479f5ef2a640a202026b6691cfa5be
Hao Nguyen <hao.ict56@gmail.com>
2020-02-19 14:15:54 +0900

    Support PWA

--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,33 @@ import envConfig from '../src/config/env-config';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang='en'>
         <Head>
+          {/* Must */}
           <meta charSet='utf-8' />
-          <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
-          <link rel='icon' type='image/x-icon' href='/static/assets/images/favicon.ico?v=1' />
+          <meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1, shrink-to-fit=no' />
+          <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+          <link rel='icon' type='image/x-icon' href='/static/assets/images/favicon.ico' />
+          <meta name='description' content='Next generation of Dictionaries!!' />
+          <meta name='keywords' content='dictionary' />
+          <link rel='manifest' href='/manifest.json' />
+
+          {/* Android */}
+          <meta name='theme-color' content='#c40012' />
+          <meta name='mobile-web-app-capable' content='yes' />
+
+          {/* iOS */}
+          <meta name='apple-mobile-web-app-title' content='Next generation of Dictionaries!!' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <link rel='apple-touch-icon' href='/static/assets/images/favicon.ico'></link>
+
+          {/* Windows */}
+          <meta name='msapplication-navbutton-color' content='#c40012' />
+          <meta name='msapplication-TileColor' content='#c40012' />
+          <meta name='msapplication-TileImage' content='/static/assets/images/favicon.ico' />
+          {/* <meta name='msapplication-config' content='browserconfig.xml' /> */}
+
           <script
             dangerouslySetInnerHTML={{
               __html: `window.apiUrl="${envConfig.app.apiUrl}"`
