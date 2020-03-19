3a06b0741b0fe955590c6915bcdb423308b5a590
Hao Nguyen <hao.ict56@gmail.com>
2020-03-20 04:18:03 +0900

    Removes legacy next-css

--- next.config.js
+++ next.config.js
@@ -1,24 +1,24 @@
 const withLess = require('@zeit/next-less');
-const withCss = require('@zeit/next-css');
 const withPWA = require('next-pwa');
 
-module.exports = withCss(
-  withLess(
-    withPWA({
-      pwa: {
-        register: false,
-        skipWaiting: false,
-        dest: 'public'
-      },
-      // only for dev
-      onDemandEntries: {
-        maxInactiveAge: 1000 * 30 * 60,
-        pagesBufferLength: 10
-      },
-      webpack: config => {
-        config.node = { fs: 'empty' };
-        return config;
-      }
-    })
-  )
+const dev = process.env.NODE_ENV !== 'production';
+
+module.exports = withLess(
+  withPWA({
+    pwa: {
+      disable: dev,
+      register: false,
+      skipWaiting: false,
+      dest: 'public'
+    },
+    // only for dev
+    onDemandEntries: {
+      maxInactiveAge: 1000 * 30 * 60,
+      pagesBufferLength: 10
+    },
+    webpack: config => {
+      config.node = { fs: 'empty' };
+      return config;
+    }
+  })
 );
